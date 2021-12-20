4fa7323ef53d721d9021b01c8f667405d912d89e
mammadlinurlan <nurlanym@code.edu.az>
2021-12-20 22:21:20 +0400

    1st

---
+++ task3/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using task3.Models;
+
+namespace task3.Controllers
+{
+    public class HomeController:Controller
+    {
+
+        public ActionResult Index()
+        {
+            List<Product> products = new List<Product>
+            {
+
+                new Product()
+                {
+                    Id = 1,
+                     ImageSrc = "dinocase.jpeg",
+                      Title = "iPhone 11",
+                       Desc = "SWEET DINO CASE",
+                        Price = "15 azn"
+                },
+                  new Product()
+                {
+                    Id = 2,
+                     ImageSrc = "dylanbrian.jpeg",
+                      Title = "iPhone X",
+                       Desc = "DYLAN O`BRIEN CASE",
+                        Price = "15 azn"
+                },
+                    new Product()
+                {
+                    Id = 3,
+                     ImageSrc = "samsungmodel1.jpeg",
+                      Title = "SAMSUNG A30",
+                       Desc = "FAMOUS PARENTAL ADVISORY CASE",
+                        Price = "15 azn"
+                },
+                      new Product()
+                {
+                    Id = 4,
+                     ImageSrc = "songip11.jpeg",
+                      Title = "iPhone 11",
+                       Desc = "SONG PLAYER CASE",
+                        Price = "15 azn"
+                }
+                 
+
+
+
+            };
+
+
+
+            return View(products);
+        }
+        
+    }
+}
---
+++ task3/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace task3.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ImageSrc { get; set; }
+        public string Title { get; set; }
+        public string Desc { get; set; }
+        public string Price { get; set; }
+
+    }
+}
